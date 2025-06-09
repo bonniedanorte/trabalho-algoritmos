4131ae5da3f5bb91f4123966c11bedc369b3fc93
Bruno <bruno_fernandes.7@hotmail.com>
2025-06-09 19:28:33 -0300

    'removendo'

--- clienterestaurante/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿decimal[] conta = new decimal[10];
-
-for (int i = 0; i < 10;  i++)
-{
-    Console.WriteLine("Digite o valor da conta" + (i + 1));
-    conta[i] = Convert.ToDecimal(Console.ReadLine());
-}
-
-Console.WriteLine("Hoje o seu almoço é um cortesia da casa, parabéns");
