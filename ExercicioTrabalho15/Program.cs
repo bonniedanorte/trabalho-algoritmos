f152d1d675b540efe104b96a77bf71d60ba30d8d
Bruno <bruno_fernandes.7@hotmail.com>
2025-06-09 19:44:41 -0300

    save

---
+++ ExercicioTrabalho15/Program.cs
@@ -0,0 +1,17 @@
+﻿decimal valorFelipe, valorConta, valorCarlos, valorAndre;
+int valorBase;
+
+Console.WriteLine("Digite o valor da conta: ");
+valorConta = decimal.Parse.(Console.ReadLine());
+
+valorBase = (int)valorConta; / 3;
+valorCarlos = (int)valorBase;
+valorAndre = (int)valorBase;
+// (int) é usado para converter o valor decimal para inteiro,descartando a parte fracionária.
+valorFelipe = valorConta - valorCarlos + valorAndre;
+
+Console.WriteLine($"Valor que Felipe deve pagar: {valorFelipe:C}");
+Console.WriteLine($"Valor que Carlos deve pagar: {valorCarlos:C}");
+Console.WriteLine($"Valor que Andre deve pagar: {valorAndre:C}");
+
+// O comando :C formata o valor como moeda, usando a cultura do sistema.
